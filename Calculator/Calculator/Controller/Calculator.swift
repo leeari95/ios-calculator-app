6e6e25990dece9bf673231c0f552e991934f0616
Ari <lee_ari95@icloud.com>
2021-11-25 00:21:37 +0900

    feat: 계산을 담당할 Calculator 타입과 CalculatorDelegate 프로토콜 생성 #7

---
+++ Calculator/Calculator/Controller/Calculator.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Ari on 2021/11/24.
+//
+
+import Foundation
+import OSLog
+
+protocol CalculatorDelegate {
+
+}
+
+class Calculator {
+    
+}
